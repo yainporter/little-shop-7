1205723c2966d27af5acf1501758077fd756c289
lance butler <lancej2004@gmail.com>
2024-02-21 15:47:43 -0700

    creates all required relationships

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,7 @@
 class Invoice < ApplicationRecord
+  belongs_to :customer
+  has_many :invoice_items
+  has_many :transactions
+  
   enum status: {"in progress" => 0, "completed" => 1, "cancelled" => 2}
 end
