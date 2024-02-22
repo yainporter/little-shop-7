5d75774e14b99051e6b4f8a39ff308b1927d5eae
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-02-21 20:02:25 -0700

    fix: Remove the comment at the top of spec/factories, and make sure that Faker syntax is correct

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,11 +1,6 @@
-#require 'rails_helper'
-
 FactoryBot.define do
   factory :customer do
     first_name { Faker::Name.first_name }
     last_name  { Faker::Name.last_name }
   end
 end
-
-
-  
--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,10 +1,6 @@
-#require 'rails_helper'
-
 FactoryBot.define do
   factory :invoice do
-    quantity { Faker::Name.quanity }
-    unit_price { Faker::Number.between(from: 100, to: 100000) }
-    status { 0 } 
+    status { 0 }
     association :customer
   end
 end
