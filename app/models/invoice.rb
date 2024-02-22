e2cd2d2b724b3ad05faa4dd048d171bced81f882
LJ9332 <149629952+LJ9332@users.noreply.github.com>
2024-02-22 02:27:48 +0000

    Fix: updated spelling error

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,5 +5,5 @@ class Invoice < ApplicationRecord
   
   validates :status, presence: true
 
-  enum status: {"in progress" => 0, "completed" => 1, "canceled" => 2}
+  enum status: {"in progress" => 0, "completed" => 1, "cancelled" => 2}
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Invoice, type: :model do
   describe "validations" do
     it { should validate_presence_of :status}
-    it { should define_enum_for(:status).with_values("in progress" => 0, "completed" => 1, "canceled" => 2)}
+    it { should define_enum_for(:status).with_values("in progress" => 0, "completed" => 1, "cancelled" => 2)}
   end
 
   describe "relationships" do
