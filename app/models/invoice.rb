f539a359baa816c243f258d27987dbc6e2b31b70
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-21 19:42:01 -0600

    feat: model validations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,5 +3,7 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :transactions
   
+  validates :status, presence: true, numericality: true
+  
   enum status: {"in progress" => 0, "completed" => 1, "canceled" => 2}
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,10 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
+
+  validates :quantity, presence: true, numericality: true
+  validates :unit_price, presence: true, numericality: true
+  validates :status, presence: true, numericality: true
   
   enum status: {"pending" => 0, "packaged" => 1, "shipped" => 2}
 end
