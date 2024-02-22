3b38cbb4cc9532b2b273b41463d62c76af422b49
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-21 21:01:48 -0600

    test: admin dashboard links

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
     resources :admin, only: [:index]
+  #merch
+    get "/admin/merchants", to: "admin#index"
 end
--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -3,11 +3,32 @@ require "rails_helper"
 RSpec.describe "Admin Dashboard", type: :feature do
   describe "As a visitor" do
     before do
+      # When I visit the admin dashboard (/admin)
       visit "/admin"
     end
-
+    
+    # 19. Admin Dashboard
     it "displays an 'Admin' page header" do
+      # As an admin,
+      # Then I see a header indicating that I am on the admin dashboard
       expect(page).to have_content("Admin Dashboard")
+    end
+
+  #20. Admin Dashboard Links
+  describe "page links" do
+    it "displays a merchant link"  do
+      
+      expect(page).to have_link("merchant")
+      click_on "merchant"
+      expect(current_path).to be("/admin/merchants")
+    end
+
+    it "displays a invoice link" do
+      
+      expect(page).to have_link("invoice")
+      click_on "invoice"
+      expect(current_path).to be("/admin/invoices")
+      
     end
   end
 end
