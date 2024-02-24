1c4ba8e20e4d205ea03523edcba857e8b2150098
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-24 15:59:40 -0600

    Test: Finished test for list invoice and customer full name

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -7,16 +7,26 @@ RSpec.describe "Admin Invoices Show", type: :feature do
       @customer_2 = Customer.create!(first_name: "Jess" last_name: "K")
 
       @invoice_1 = @customer_1.invoices.create!(status: 1, created_at: "2011-09-13")
-      @invoice_2 = create(:invoice, customer: @customer_2, status: 0, created_at: "2019-07-18")
+      @invoice_2 = @customer_2.invoices.create!(status: 2, created_at: "2022-03-08")
     end
 
     describe "User Story 33 - Admin Invoice Show page" do
       it "list invoice attributes" do
         visit admin_invoice_path(@invoice_1)
+
+        expect(page).to have_content(@invoice_1.id)
+        expect(page).to have_content("Completedfea")
+        expect(page).to have_content("Tuesday, September 13, 2011")
+        expect(page).to_not have_content(@invoice_2.id)
+        expect(page).to_not have_content("Cancelled")
+        expect(page).to_not have_content("Tuesday, September 13, 2011")
       end
 
       it "has customers first and last name" do
         visit admin_invoice_path(@invoice_1)
+
+        expect(page).to have_content("Lance B")
+        expect(page).to_not have_content("Jess K")
       end
     end
   end
