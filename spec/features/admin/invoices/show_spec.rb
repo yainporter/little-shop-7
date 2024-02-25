cc3cb4ba25fce1b5a6ba477a018a15593eb4feeb
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-24 21:50:19 -0600

    test: invoice's item info

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -3,11 +3,25 @@ require "rails_helper"
 RSpec.describe "Admin Invoices Show", type: :feature do
   describe "As a admin" do
     before do
+      @merchant_1 = create(:merchant)
+
+      @item_1 = create(:item, name: "shoes", merchant: @merchant_1)
+      @item_2 = create(:item, name: "books", merchant: @merchant_1)
+      @item_3 = create(:item, name: "lamp", merchant: @merchant_1)
+
       @customer_1 = Customer.create!(first_name: "Lance", last_name: "B")
       @customer_2 = Customer.create!(first_name: "Jess", last_name: "K")
 
       @invoice_1 = @customer_1.invoices.create!(status: 1, created_at: "2011-09-13")
       @invoice_2 = @customer_2.invoices.create!(status: 2, created_at: "2022-03-08")
+
+      @invoice_item_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 1, unit_price: 2500, status: 0) # pending
+      @invoice_item_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 2, unit_price: 1000, status: 1) # packaged
+      @invoice_item_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 3, unit_price: 100000, status: 2) # shipped
+
+      # unit_price
+        # price sold at -> invoice_items
+        # current sales price -> item
     end
 
     describe "User Story 33 - Admin Invoice Show page" do
@@ -27,6 +41,31 @@ RSpec.describe "Admin Invoices Show", type: :feature do
 
         expect(page).to have_content("Lance B")
         expect(page).to_not have_content("Jess K")
+      end
+    end
+
+    describe "User Story 34 - Invoice Item Information" do
+      it "lists invoice's items and their info" do
+        within "#item-#{@item_1.id}" do
+          expect(page).to have_content("shoes")   # item name
+          expect(page).to have_content("1")       # quantity ordered
+          expect(page).to have_content("$15")     # sold at
+          expect(page).to have_content("Pending") # status
+        end
+
+        within "#item-#{@item_2.id}" do
+          expect(page).to have_content("shoes")
+          expect(page).to have_content("2")
+          expect(page).to have_content("$15")
+          expect(page).to have_content("Pending")
+        end
+
+        within "#item-#{@item_3.id}" do
+          expect(page).to have_content("shoes")
+          expect(page).to have_content("3")
+          expect(page).to have_content("$15")
+          expect(page).to have_content("Pending")
+        end
       end
     end
   end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -9,11 +9,29 @@ RSpec.describe InvoiceItem, type: :model do
     it { should validate_numericality_of :unit_price}
     
     it { should validate_presence_of :status}
-    it { should define_enum_for(:status).with_values("pending" => 0, "packaged" => 1, "shipped" => 2)}
+    it { should define_enum_for(:status).with_values("Pending" => 0, "Packaged" => 1, "Shipped" => 2)}
   end
-
+  
   describe "relationships" do
     it {should belong_to :invoice}
     it {should belong_to :item}
+  end
+
+  before do
+    @merchant_1 = create(:merchant)
+    @item_1 = create(:item, merchant: @merchant_1)
+    @item_2 = create(:item, merchant: @merchant_1)
+    @customer_1 = create(:customer)
+    @invoice_1 = create(:invoice, customer: @customer_1)
+    @invoice_2 = create(:invoice, customer: @customer_1)
+    @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1, unit_price: 500)
+    @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_2, unit_price: 1027)
+  end
+
+  describe "instance methods" do
+    it "formats unit_price sold at" do
+      expect(@invoice_item_1.format_price_sold).to eq("$5.00")
+      expect(@invoice_item_2.format_price_sold).to eq("$10.27")
+    end
   end
 end
