d8d847cb0f9c5efe15331ff6534b3b46b79b99f3
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-25 00:21:09 -0600

    feat: invoice's items list with info displayed

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Admin Invoices Show", type: :feature do
       @merchant_1 = create(:merchant)
 
       @item_1 = create(:item, name: "shoes", merchant: @merchant_1)
-      @item_2 = create(:item, name: "books", merchant: @merchant_1)
+      @item_2 = create(:item, name: "book", merchant: @merchant_1)
       @item_3 = create(:item, name: "lamp", merchant: @merchant_1)
 
       @customer_1 = Customer.create!(first_name: "Lance", last_name: "B")
@@ -16,18 +16,14 @@ RSpec.describe "Admin Invoices Show", type: :feature do
       @invoice_2 = @customer_2.invoices.create!(status: 2, created_at: "2022-03-08")
 
       @invoice_item_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 1, unit_price: 2500, status: 0) # pending
-      @invoice_item_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 2, unit_price: 1000, status: 1) # packaged
-      @invoice_item_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 3, unit_price: 5000, status: 2) # shipped
+      @invoice_item_2 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_1.id, quantity: 2, unit_price: 1000, status: 1) # packaged
+      @invoice_item_3 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_1.id, quantity: 3, unit_price: 5000, status: 2) # shipped
 
-      # unit_price
-        # price sold at -> invoice_items
-        # current sales price -> item
+      visit admin_invoice_path(@invoice_1)
     end
 
     describe "User Story 33 - Admin Invoice Show page" do
       it "list invoice attributes" do
-        visit admin_invoice_path(@invoice_1)
-
         expect(page).to have_content(@invoice_1.id)
         expect(page).to have_content("Completed")
         expect(page).to have_content("Tuesday, September 13, 2011")
@@ -37,8 +33,6 @@ RSpec.describe "Admin Invoices Show", type: :feature do
       end
 
       it "has customers first and last name" do
-        visit admin_invoice_path(@invoice_1)
-
         expect(page).to have_content("Lance B")
         expect(page).to_not have_content("Jess K")
       end
@@ -46,25 +40,25 @@ RSpec.describe "Admin Invoices Show", type: :feature do
 
     describe "User Story 34 - Invoice Item Information" do
       it "lists invoice's items and their info" do
-        within "#item-#{@item_1.id}" do
-          expect(page).to have_content("shoes")   # item name
-          expect(page).to have_content("1")       # quantity ordered
-          expect(page).to have_content("$25.00")     # sold at
-          expect(page).to have_content("Pending") # status
+        within "#invoice_item-#{@invoice_item_1.id}" do
+          expect(page).to have_content("shoes")
+          expect(page).to have_content("1")
+          expect(page).to have_content("$25.00")
+          expect(page).to have_content("Pending")
         end
 
-        within "#item-#{@item_2.id}" do
-          expect(page).to have_content("shoes")
+        within "#invoice_item-#{@invoice_item_2.id}" do
+          expect(page).to have_content("book")
           expect(page).to have_content("2")
           expect(page).to have_content("$10.00")
-          expect(page).to have_content("Pending")
+          expect(page).to have_content("Packaged")
         end
 
-        within "#item-#{@item_3.id}" do
-          expect(page).to have_content("shoes")
+        within "#invoice_item-#{@invoice_item_3.id}" do
+          expect(page).to have_content("lamp")
           expect(page).to have_content("3")
           expect(page).to have_content("$50.00")
-          expect(page).to have_content("Pending")
+          expect(page).to have_content("Shipped")
         end
       end
     end
