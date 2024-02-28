90a9bb6d265e03dc230e511c8a5183c76eeeb781
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-02-28 12:28:53 -0700

    feat: Complete checking off advanced routing, except Admin Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -14,32 +14,7 @@ Rails.application.routes.draw do
   resources :admin, only: [:index]
 
   namespace :admin do
-    resources :merchants, except: [:destroy]
+    resources :merchants, except: [:destroy], via: [:patch]
     resources :invoices, only: [:index, :show, :update], via: [:patch]
   end
 end
-#           merchant_items GET   /merchants/:merchant_id/items(.:format)                                                           merchant/items#index
-#       edit_merchant_item GET   /merchants/:merchant_id/items/:id/edit(.:format)                                                  merchant/items#edit
-#            merchant_item GET   /merchants/:merchant_id/items/:id(.:format)                                                       merchant/items#show
-#                          PATCH /merchants/:merchant_id/items/:id(.:format)                                                       merchant/items#update
-
-# Prefix Verb  URI Pattern                                                                                       Controller#Action
-#                          POST  /merchants/:merchant_id/items(.:format)                                                           merchant/items#create
-#        new_merchant_item GET   /merchants/:merchant_id/items/new(.:format)                                                       merchant/items#new
-#                          PUT   /merchants/:merchant_id/items/:id(.:format)                                                       merchant/items#update
-#        merchant_invoices GET   /merchants/:merchant_id/invoices(.:format)                                                        merchant/invoices#index
-#         merchant_invoice GET   /merchants/:merchant_id/invoices/:id(.:format)                                                    merchant/invoices#show
-#    merchant_invoice_item PATCH /merchants/:merchant_id/invoice_items/:id(.:format)                                               merchant/invoice_items#update
-#                          PUT   /merchants/:merchant_id/invoice_items/:id(.:format)                                               merchant/invoice_items#update
-#              admin_index GET   /admin(.:format)                                                                                  admin#index
-#          admin_merchants GET   /admin/merchants(.:format)                                                                        admin/merchants#index
-#                          POST  /admin/merchants(.:format)                                                                        admin/merchants#create
-#       new_admin_merchant GET   /admin/merchants/new(.:format)                                                                    admin/merchants#new
-#      edit_admin_merchant GET   /admin/merchants/:id/edit(.:format)                                                               admin/merchants#edit
-#           admin_merchant GET   /admin/merchants/:id(.:format)                                                                    admin/merchants#show
-#                          PATCH /admin/merchants/:id(.:format)                                                                    admin/merchants#update
-#                          PUT   /admin/merchants/:id(.:format)                                                                    admin/merchants#update
-#           admin_invoices GET   /admin/invoices(.:format)                                                                         admin/invoices#index
-#            admin_invoice GET   /admin/invoices/:id(.:format)                                                                     admin/invoices#show
-#                          PATCH /admin/invoices/:id(.:format)                                                                     admin/invoices#update
-#                          PUT   /admin/invoices/:id(.:format)                                                                     admin/invoices#update
