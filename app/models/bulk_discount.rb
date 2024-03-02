3213bca38f6a11bbfdaf1d1d2fdd17d3ac6d0a61
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-02 14:40:39 -0700

    feat: Add more specific testing

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,6 +2,6 @@ class BulkDiscount < ApplicationRecord
   belongs_to :merchant
 
   validates :name, presence: true
-  validates :percentage, numericality: true
-  validates :quantity_threshold, numericality: true
+  validates :percentage, numericality: { less_than: 0, greater_than: -1 }
+  validates :quantity_threshold, numericality: { only_integer: true }
 end
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe BulkDiscount, type: :model do
   describe "validations "do
     it { should validate_presence_of :name}
-    it { should validate_numericality_of :percentage}
-    it { should validate_numericality_of :quantity_threshold}
+    it { should validate_numericality_of(:percentage).is_less_than(0).is_greater_than(-1)}
+    it { should validate_numericality_of(:quantity_threshold).only_integer}
   end
 
   describe "relationships" do
