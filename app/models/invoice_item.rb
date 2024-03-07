594250b3c0fb6078224db22284d56291069891bb
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-06 20:08:08 -0700

    feat: Add validation for InvoiceItem and Merchants

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,7 +2,7 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
   has_many :merchants, through: :item
-  has_many :bulk_discounts, through: :merchants
+
 
   validates :quantity, presence: true, numericality: true
   validates :unit_price, presence: true, numericality: true
